90c6e70cfdb11d806be4bcc81921e29b5ea2b67e
Yogesh <ykhinchi38@gmail.com>
2025-05-26 11:09:56 +0530

    live url added

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const mongoUrl = 'mongodb://localhost:27017/hotel'
+//const mongoUrl = 'mongodb://localhost:27017/hotel'
+const mongoUrl = 'mongodb+srv://Yogesh:unity7877@cluster0.zirgoia.mongodb.net/'
 
 mongoose.connect(mongoUrl , {
     useNewUrlParser : true ,
